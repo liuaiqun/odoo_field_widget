2f7cc1b6c864b23b95fde4b40f4f647b53e9299a
liuaiqun <liuaiqun@sina.com>
2019-08-10 00:31:44 +0800

    add another_widget_integer folder

---
+++ another_widget_integer/__manifest__.py
@@ -0,0 +1,16 @@
+
+{
+    "name": "Another Field widget of Integer",
+    "summary": "Reimppetion  Integer field widget of odoo web module "
+               "for learn odoo web client programming .",
+    "category": "Web",
+    "version": "12.0.0.0.0",
+    "license": "LGPL-3",
+    "author": "odoo",
+    "website": "https://github.com/odoo/web/static/src/js/fields/basic_fields.js",
+    "depends": ["web"],
+    "data": [
+        "templates/assets.xml",
+    ],
+    "installable": True,
+}
---
+++ another_widget_integer/static/src/js/another_integer_widget.js
@@ -0,0 +1,56 @@
+
+
+odoo.define('another_widget_integer.AnotherFieldInteger', function (require) {
+"use strict";
+
+
+    var basic_fields = require('web.basic_fields')
+    var core = require('web.core');
+    var registry = require('web.field_registry');
+    var QWeb = core.qweb;
+    var _t = core._t;
+    
+    
+    
+    var FieldAnotherInteger = basic_fields.NumericField.extend({
+        className: 'o_field_integer o_field_number',
+        supportedFieldTypes: ['integer'], 
+
+        //--------------------------------------------------------------------------
+        // Private
+        //--------------------------------------------------------------------------
+
+        /**
+        * Format integer value
+        *
+        * Note: We have to overwrite this method to allow virtual ids. A virtual id
+        * is a character string composed of an integer and has a dash and other
+        * information.
+        * E.g: in calendar, the recursive event have virtual id linked to a real id
+        * virtual event id "23-20170418020000" is linked to the event id 23
+        *
+        * @override
+        * @private
+        * @param {integer|string} value
+        * @returns {string}
+        */
+        _formatValue: function (value) {
+            
+            console.log("another integer: formatValue")
+            if (typeof value === 'string') {
+                if (!/^[0-9]+-/.test(value)) {
+                    throw new Error('"' + value + '" is not an integer or a virtual id');
+                }
+                return value;
+            }
+            return this._super.apply(this, arguments);
+        },
+    });
+
+
+    registry.add('another_integer', FieldAnotherInteger);
+});
+
+
+
+

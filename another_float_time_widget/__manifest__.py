69ea9356fa87618bc050c6ab4bd5d9dc0b3551b4
liuaiqun <liuaiqun@sina.com>
2019-08-17 16:04:07 +0800

    add module test_another_widget

---
+++ another_float_time_widget/__manifest__.py
@@ -0,0 +1,15 @@
+
+{
+    "name": "Another FloatTime Widget",
+    "description": "Reimplementation  of odoo  basic field widget -- FloatTime",
+    "category": "Web",
+    "version": "12.0.0.0.0",
+    "license": "LGPL-3",
+    "author": "odoo",
+    "website": "https://github.com/odoo/web/static/src/js/fields/basic_fields.js",
+    "depends": ["web"],
+    "data": [
+        "templates/assets.xml",
+    ],
+    "installable": True,
+}
---
+++ another_float_time_widget/static/src/js/another_float_time_widget.js
@@ -0,0 +1,35 @@
+
+
+odoo.define('another_float_time_widget.another_field_float_time', function (require) {
+"use strict";
+
+
+    var basic_fields = require('web.basic_fields')
+    var core = require('web.core');
+    var registry = require('web.field_registry');
+    var QWeb = core.qweb;
+    var _t = core._t;
+
+    
+    var AnotherFieldFloatTimw = basic_fields.FieldFloat.extend({
+        
+        // this is not strictly necessary, as for this widget to be used, the 'widget'
+        // attrs must be set to 'float_time', so the formatType is automatically
+        // 'float_time', but for the sake of clarity, we explicitely define a
+        // FieldFloatTime widget with formatType = 'float_time'.
+        formatType: 'float_time',
+
+        init: function () {
+            console.log("[another_float_time_widget.js] function:init()")
+            this._super.apply(this, arguments);
+            this.formatType = 'float_time';
+        }
+    });
+
+
+    registry.add('another_float_time', AnotherFieldFloatTimw);
+});
+
+
+
+

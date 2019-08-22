aa1b4fe7a172a43079fd8234001a7e2945b18663
liuaiqun <liuaiqun@sina.com>
2019-08-22 09:43:34 +0800

    add format function to another_percentage_widget

--- another_percentage_widget/static/src/js/another_percentage_widget.js
+++ another_percentage_widget/static/src/js/another_percentage_widget.js
@@ -3,7 +3,7 @@
 odoo.define('another_percentage_widget.another_field_percentage', function (require) {
 "use strict";
 
-
+    var field_utils = require('web.field_utils');
     var basic_fields = require('web.basic_fields')
     var core = require('web.core');
     var registry = require('web.field_registry');
@@ -11,19 +11,61 @@ odoo.define('another_percentage_widget.another_field_percentage', function (requ
     var _t = core._t;
     
     
+     /**==========================================================
+     * widget定义
+     * from  odoo\addons\web\static\src\js\fields\basic_fields.js
+     ==========================================================*/
+    
     var AnotherFieldPercentage = basic_fields.FieldFloat.extend({
         formatType:'percentage',
         
         
-        init: function () {
-            console.log("[another_field_percentage.js] function:init()")
-            this._super.apply(this, arguments);
-            //增加如下一行：因为定义的formatType属性未生效  
-            this.formatType = 'percentage';
-        
+        //init: function () {
+        //    console.log("[another_field_percentage.js] function:init()")
+        //    this._super.apply(this, arguments);
+        //    //增加如下一行：因为定义的formatType属性未生效  
+        //    this.formatType = 'percentage';
+
     });
         
+
+    
+    
+    /**==========================================================
+     * 格式化
+     * Ref： odoo\addons\web\static\src\js\fields\field_utils.js
+     ==========================================================*/
+     
+    
+    /**
+     * Returns a string representing the given value (multiplied by 100)
+     * concatenated with '%'.
+     *
+     * @param {number | false} value
+     * @param {Object} [field]
+     * @param {Object} [options]
+     * @param {function} [options.humanReadable] if returns true, parsing is avoided
+     * @returns {string}
+     */
+    function AnotherFormatPercentage(value, field, options) {
+        options = options || {};
+        var result = field_utils.format.float(value * 100, field, options) || '0';
+        if (options.humanReadable && options.humanReadable(value * 100)) {
+            return result + "%";
+        }
+        return field_utils.parse.float(result) + "%";
+    };
+    
+
+
+    
+    /**==========================================================
+     * 注册
+     * Ref：  odoo\addons\web\static\src\js\fields\field_registry.js
+     ==========================================================*/
     registry.add('another_percentage', AnotherFieldPercentage);
+    field_utils.format['another_percentage']=AnotherFormatPercentage;
+    console.log("[another_field_percentage.js] registry over ");
 });
 
 

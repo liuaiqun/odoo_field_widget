3d7164c5958aabeae4a35c2b8629a064b319b556
liuaiqun <liuaiqun@sina.com>
2019-08-27 21:05:50 +0800

    add features for module another_phone_widget

--- another_phone_widget/__manifest__.py
+++ another_phone_widget/__manifest__.py
@@ -1,7 +1,18 @@
-
+# -*- coding: utf-8 -*-
 {
     "name": "Another Phone Widget",
-    "description": "Reimplementation  of odoo  basic field widget -- Phone",
+    "summary": "Reimplementation  of odoo  basic field widget -- Phone",
+    "description": """ 
+               重新实现odoo基本字段的小部件 -- phone
+               
+               本系列(another_xxxxxx)代码取自 odoo的web模块，供学习分析odoo前端代码使用
+               
+               与odoo原phone widget相比， 本模块做了如下修改
+               
+                  ---- 将手机号码13912345678 显示为 13912xxx678
+                  
+                  ---- 强制使用readonly方式  
+                """ ,  
     "category": "Web",
     "version": "12.0.0.0.0",
     "license": "LGPL-3",
--- another_phone_widget/static/src/js/another_phone_widget.js
+++ another_phone_widget/static/src/js/another_phone_widget.js
@@ -7,33 +7,43 @@ odoo.define('another_phone_widget.another_field_phone', function (require) {
     var QWeb = core.qweb;
     var _t = core._t;
     
+    
+     /**==========================================================
+     * widget定义
+     * from  odoo\addons\web\static\src\js\fields\basic_fields.js
+     ==========================================================*/
+    
     var AnotherFieldPhone = basic_fields.FieldEmail.extend({
         
         className: 'o_field_another_phone',
         prefix: 'tel',
 
-        //--------------------------------------------------------------------------
-        // Private
-        //--------------------------------------------------------------------------
-
-        /**
-         * @override
-         * @private
-         */
-        _renderReadonly: function () {
-            this._super();
-            
-            console.log("[another_phone_widget.js] function:_renderReadonly()")
-
-            // This class should technically be there in case of a very very long
-            // phone number, but it breaks the o_row mechanism, which is more
-            // important right now.
-            this.$el.removeClass('o_text_overflow');
+        init: function () {
+            arguments[3].mode="readonly";
+            this._super.apply(this, arguments);
+            //this.attrs.readonly= true;
+            console.log("[another_phone_widget.js] function:init()")
         },
+         
+        _renderReadonly: function () {
+            var phone_str=this.value.substr(0,5)+"xxx"+this.value.substr(8,99)
+            this.$el.text(phone_str)
+                .addClass('o_readonly_modifier');
+            console.log("[another_phone_widget.js] function:_renderReadonly()")
+        },
+
+
+        
     });
 
-
+     
+     
+    /**==========================================================
+     * 注册
+     * Ref：  odoo\addons\web\static\src\js\fields\field_registry.js
+     ==========================================================*/
     registry.add('another_phone', AnotherFieldPhone);
+    console.log("[another_phone_widget.js] registry over ");
 });
 
 
